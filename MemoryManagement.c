d61c6ec4c5298e24c158c7774195a2feddc1f307
Joey <gallottaj@hotmail.com>
2020-10-22 12:54:52 -0700

    initial commit

---
+++ MemoryManagement.c
@@ -0,0 +1,17 @@
+/////Joey Gallotta
+////CST-221 Memory Management
+////10/25/2020
+ 
+//Program that takes a decimal number between 0 and 4095 as input from a user that performs the following operations on the input:
+//
+//Display the number as a 32-bit binary number displaying each binary digit as a 1 or 0 (with the least significant bit on right) on the console. 
+//
+//Display the number as a 32-bit hexadecimal number on the console. 
+//
+//Shift the number 16 bits to the left, then mask out (AND) the bottom 16 bits, and finally add (OR) the hex number 0x07FF to produce the final resultant number. Display the final result in binary, hexadecimal, and decimal to the console.d.  
+
+//Write your program in a modular fashion to eliminate redundant code.
+
+
+
+
